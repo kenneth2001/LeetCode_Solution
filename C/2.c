261bb88b4c868967afc9d32851ec9381973f1041
YeeKiiiiii <yeeki0904@gmail.com>
2021-04-06 16:41:06 +0800

    Update 2.c

--- C/2.c
+++ C/2.c
@@ -1,22 +1,21 @@
 struct ListNode {
     int val;
-    struct ListNode * next;
+    struct ListNode* next;
 };
 
-
-struct ListNode * addTwoNumbers(struct ListNode * l1, struct ListNode * l2){
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
     newNode->val = 0;
     newNode->next = NULL;
     struct ListNode* currentNode = newNode;
     int carry = 0, val;
-    while (l1 || l2){
+    while (l1 || l2) {
         val = carry;
-        if (l1){
+        if (l1) {
             val += l1->val;
             l1 = l1->next;
         }
-        if (l2){
+        if (l2) {
             val += l2->val;
             l2 = l2->next;
         }
@@ -27,13 +26,13 @@ struct ListNode * addTwoNumbers(struct ListNode * l1, struct ListNode * l2){
         currentNode->next = node;
         currentNode = currentNode->next;
     }
-    
-    if (carry == 1){
+
+    if (carry == 1) {
         struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
         node->val = 1;
         node->next = NULL;
         currentNode->next = node;
     }
-    
+
     return newNode->next;
 }
